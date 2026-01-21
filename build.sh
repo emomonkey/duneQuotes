bced8b2ddb68e66a2ee6b26aa9925ba7f5edda4e
emomonkey <stephen.emo@gmail.com>
2026-01-21 20:51:40 +0000

    Fix base64 checksum in Gemfile.lock

--- build.sh
+++ build.sh
@@ -7,4 +7,12 @@ development:
       options:
         auth_mech: :scram
         server_selection_timeout: 5
+
+production:
+  clients:
+    default:
+      uri: "$MONGODB_URI"
+      options:
+        auth_mech: :scram
+        server_selection_timeout: 5
 EOF
